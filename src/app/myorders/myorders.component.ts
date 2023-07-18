7ef5a3954da5063d8ef04eb5d441be955b7f52ff
unknown <amit423raja@gmail.com>
2023-07-18 21:53:08 +0530

    my order

--- src/app/myorders/myorders.component.ts
+++ src/app/myorders/myorders.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
+import {HttpClient } from '@angular/common/http'
 import { AuthService } from '../services/auth.service';
 import { OrderService } from '../services/order.service';
+import { environment } from 'src/environments/environment';
 
 export interface PeriodicElement {
   name: string;
@@ -10,11 +12,11 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
 ];
 @Component({
   selector: 'app-myorders',
@@ -23,8 +25,8 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class MyordersComponent implements OnInit {
   progress = true;
-  tableData : any = ELEMENT_DATA;
-   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  tableData: any = ELEMENT_DATA;
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   dayMap = new Map(
     [
       [0, "Sunday"],
@@ -32,25 +34,33 @@ export class MyordersComponent implements OnInit {
       [2, "Tuesday"],
       [3, "Wednesday"],
       [4, "Thursday"],
-      [5,"Friday"],
-      [6,"Saturday"]
+      [5, "Friday"],
+      [6, "Saturday"]
     ])
 
-  myorders:any = [];
-  constructor(private auth : AuthService, private order : OrderService) { }
+  myorders: any = [];
+  constructor(private auth: AuthService, private order: OrderService, private http : HttpClient) { }
 
   ngOnInit(): void {
     this.order.getMyOrders().subscribe(res => {
       console.log(res);
       this.myorders = res;
-    },err => {
+    }, err => {
       this.progress = false;
-    },() => {
+    }, () => {
       this.progress = false
 
     })
   }
 
-  cancelOrder(){}
+  cancelOrder(order : any) { 
+    console.log(order);
+    
+     this.http.put(environment.domain + '/order/cancel',{id:order.id}).subscribe(res => {
+
+     },err=>{},()=>{
+      order.status = 'CANCELLED'
+     })
+  }
 
 }
